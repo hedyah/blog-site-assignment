f71921ec2d1f01ad2a1faa97c970b3c137785c9f
Hedwige Uhllanda <hedwigeuhllanda@Hedwiges-MacBook-Air.local>
2022-06-01 20:22:47 -0600

    added the functions for the blog

--- app.py
+++ app.py
@@ -1,24 +1,60 @@
 
-from select import select
+import dbcreds
+import mariadb
 
 
-print("Welcome to our Blog")
+conn = mariadb.connect(
+                        user = dbcreds.user,
+                        password = dbcreds.password,
+                        host = dbcreds.host,
+                        port = dbcreds.port,
+                        database = dbcreds.database
+)
 
-user =  input("Type your username to post content to Bloogie: ")
-print ("Hey, ", user )
+cursor = conn.cursor()
+
+print("Welcome to our Blog !!!")
+
+blog_user =  input("Type your username to post content to Bloogie: ")
+print ("Welcome, ", blog_user )
 
 
-print(user, "   Here are some options for you !")
-print("1.Would you like to write a new post?")
-print("2.Would you like to view other posts?")
-
+print(blog_user, " Here are some options for you !")
+print("1.Write a new blog post?")
+print("2.View other blog posts?")
 
 while True:
-    select = input("Enter selection (1/2): ")
     
-    if select in ('1', '2'):
-        new_post = input("Would you like to write a new post?: (y/n) ")
-        see_post = input( "Would you like to see other posts?: (y/n)")
+    select = input("Enter your selection ( 1 or 2 ): ")
     
-        if select == '1':
+    if select == '1':
             
+            print("Hi, ", blog_user)
+            new_post=input("Type your content here: ")
+            print(new_post , "   Would you like to submit this post?")
+            
+            insert_statement= ("""INSERT INTO blog_post(username, content)
+                            VALUES (?,?)""") 
+            data =  blog_user, new_post 
+            
+            cursor.execute(insert_statement,data)
+            conn.commit()
+            
+            print("Content Posted Successfully!!")
+            
+    elif select == '2':
+            sql = """SELECT * FROM blog_post"""
+            cursor.execute(sql)
+            result = cursor.fetchone();
+            print(result)
+            result = cursor.fetchall();
+            print(result)
+    
+    select_next = input("Would you like to exit? (yes/no):  ")
+    if select_next == "yes":
+        break
+    
+    else:
+        print("Invalid input, try again!")
+    
+
